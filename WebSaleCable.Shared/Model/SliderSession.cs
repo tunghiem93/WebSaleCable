a65950ec480a42000b85767cfdb443886c3b0933
TuanVo <voquoctuanqn@gmail.com>
2018-04-21 09:58:49 +0700

    [+] add model sliderSession

---
+++ WebSaleCable.Shared/Model/SliderSession.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSaleCable.Shared.Model
+{
+    public class SliderSession
+    {
+        public string ImageUrl { get; set; }
+    }
+}
