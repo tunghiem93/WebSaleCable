68d2889940fa86112c5da490e7f21d386f9d20d9
TuanVo <voquoctuanqn@gmail.com>
2018-04-23 21:47:24 +0700

    [M] update home

--- WebSaleCable/Areas/ClientSite/Controllers/HomeController.cs
+++ WebSaleCable/Areas/ClientSite/Controllers/HomeController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSaleCable.Shared;
+using WebSaleCable.Shared.Factory.ProductFactory;
 using WebSaleCable.Shared.Model.Product;
 
 namespace WebSaleCable.Areas.ClientSite.Controllers
 {
     public class HomeController : HQController
     {
+        private ProductFactory _fac;
+        public HomeController()
+        {
+            _fac = new ProductFactory();
+        }
         // GET: ClientSite/Home
         public ActionResult Index()
         {
             ProductViewModels model = new ProductViewModels();
-            model.ListLoca = getListLocation();
-            model.ListCate = getListCategory();
+            model.ListProduct = _fac.GetListProduct();
+            model.ListProduct.ForEach(x =>
+            {
+                if (!string.IsNullOrEmpty(x.ImageURL))
+                    x.ImageURL = Commons.HostImage + x.ImageURL;
+            });
+            //model.ListLoca = getListLocation();
+            //model.ListCate = getListCategory();
             return View(model);
         }
 
@@ -26,7 +39,14 @@ namespace WebSaleCable.Areas.ClientSite.Controllers
         [HttpGet]
         public new PartialViewResult LoadItem(string id)
         {
-            return PartialView("_ListItem");
+            ProductViewModels model = new ProductViewModels();
+            model.ListProduct = _fac.GetListProduct().Where(x=>x.CategoryID.Equals(id)).ToList();
+            model.ListProduct.ForEach(x =>
+            {
+                if (!string.IsNullOrEmpty(x.ImageURL))
+                    x.ImageURL = Commons.HostImage + x.ImageURL;
+            });
+            return PartialView("_ListItem",model.ListProduct);
         }
     }
 }
