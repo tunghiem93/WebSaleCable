aff7b773efe7e5a126bfae311d5db6319e385288
TuanVo <voquoctuanqn@gmail.com>
2018-04-23 16:46:47 +0700

    [M] update model SliderSession

--- WebSaleCable.Shared/Model/SliderSession.cs
+++ WebSaleCable.Shared/Model/SliderSession.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using WebSaleCable.Shared.Model.Product;
 
 namespace WebSaleCable.Shared.Model
 {
     public class SliderSession
     {
         public string ImageUrl { get; set; }
+        public List<ImageProduct> ListImg { get; set; }
+        public SliderSession()
+        {
+            ListImg = new List<ImageProduct>();
+        }
     }
 }
