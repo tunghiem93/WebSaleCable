072f022720d3d5cf1bc03f13c32639f62fe416d5
TuNghiem93 <tunx406.ktl@gmail.com>
2018-04-25 01:18:18 +0700

    [+] add file; [M] Fix(layout, search), update source

---
+++ WebSaleCable/Areas/ClientSite/Controllers/PolicyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSaleCable.Areas.ClientSite.Controllers
+{
+    public class PolicyController : Controller
+    {
+        // GET: ClientSite/Policy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
