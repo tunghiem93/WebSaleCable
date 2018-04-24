37c83b70bec1557cb8fe8267e1326916b250297c
TuNghiem93 <tunx406.ktl@gmail.com>
2018-04-24 15:08:50 +0700

    [M] Update source View

--- WebSaleCable/Areas/ClientSite/Controllers/ContactController.cs
+++ WebSaleCable/Areas/ClientSite/Controllers/ContactController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ProjectWebSaleLand.Areas.ClientSite.Controllers
+namespace WebSaleCable.Areas.ClientSite.Controllers
 {
     public class ContactController : Controller
     {
