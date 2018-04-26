3324f95b2faffcc95a1326cacb11770b204d3982
TuNghiem93 <tunx406.ktl@gmail.com>
2018-04-26 12:20:15 +0700

    [M] Update source

--- WebSaleCable/Areas/Administration/Controllers/HomeController.cs
+++ WebSaleCable/Areas/Administration/Controllers/HomeController.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebSaleCable.Web.App_Start;
 
 namespace WebSaleCable.Areas.Administration.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        // GET: Administration/Home
+        [NuAuth]
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            try
+            {
+                if (Session["User"] == null)
+                    return RedirectToAction("Login", new { area = "Administration" });
+
+                FormsAuthentication.SignOut();
+                Session.Remove("User");
+
+                return RedirectToAction("Index", "Home", new { area = "Administration" });
+            }
+            catch (Exception ex)
+            {
+                NSLog.Logger.Error("Logout Error: ", ex);
+                return new HttpStatusCodeResult(400, ex.Message);
+            }
         }
     }
 }
