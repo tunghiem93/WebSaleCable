a237604208abd9c70adf955956d705190e1b5136
TuanVo <voquoctuanqn@gmail.com>
2018-05-14 10:43:08 +0700

    [M] update model

--- WebSaleCable.Shared/Model/CateSession.cs
+++ WebSaleCable.Shared/Model/CateSession.cs
@@ -8,7 +8,15 @@ namespace WebSaleCable.Shared
 {
     public class CateSession
     {
+        public List<CateSession> MainCate { get; set; }
+        public List<CateSession> OrtherCate { get; set; }
         public string Id { get; set; }
         public string Name { get; set; }
+
+        public CateSession()
+        {
+            MainCate = new List<CateSession>();
+            OrtherCate = new List<CateSession>();
+        }
     }
 }
